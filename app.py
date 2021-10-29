102817f083ae4438ccd06da03b1d2c2ce40b9a4e
jakob1111996 <jakob.kruse@jina.ai>
2021-10-29 10:46:32 +0200

    feat: finished flows - example is working

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ logger = JinaLogger('3d-showcase')
 def config():
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     workspace_dir = os.path.join(cur_dir, 'workspace')
+    data_dir = os.path.join(cur_dir, 'toy_data')
     os.environ['JINA_PORT'] = '45678'
     os.environ['WORKSPACE_MOUNT'] = f'{workspace_dir}:/workdir/workspace'
+    os.environ['DATA_MOUNT'] = f'{data_dir}:/workspace/toy_data'
 
 
 def get_docs(num_docs):
@@ -30,7 +32,7 @@ def get_docs(num_docs):
 
 @click.command()
 @click.option('--task', '-t', type=click.Choice(['index', 'query', 'query_restful']))
-@click.option('--num_data', '-n', type=int, default=100)
+@click.option('--num_data', '-n', type=int, default=1000)
 def main(task, num_data):
     config()
     flow = Flow.load_config('flow.yml')
@@ -39,9 +41,9 @@ def main(task, num_data):
         with flow:
             flow.post(on='/index', inputs=get_docs(num_data))
     elif task == 'query':
+        input_doc = Document(uri='toy_data/laptop_laptop_computer_17.glb')
         with flow:
-            results = flow.post(on='/search', return_results=True, inputs=Document(
-                uri='toy_data/laptop_laptop_computer_17.glb'))
+            results = flow.post(on='/search', return_results=True, inputs=input_doc)
             for index, match in enumerate(results[0].docs[0].matches):
                 print(f'Match {index:02d}: {match.uri}')
 
